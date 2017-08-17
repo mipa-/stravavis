de2456f099bedb8ef3f69cdafaba59bfb6ef2786
Oiva Eskola <oiva.eskola@gmail.com>
2017-08-17 22:52:52 +0300

    fix strava access token
    Get access token and add it to session. Use same
    access token when fetching new requests

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // server/app.js
 const bodyParser = require('body-parser');
 const express = require('express');
+const session = require('express-session');
 const morgan = require('morgan');
 const path = require('path');
 const stravaConfig = require('./config')
@@ -15,45 +16,48 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // parse JSON parameters
 app.use(bodyParser.json())
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET
+}))
 
 app.post('/api/handshake', (req, res) => {
   const code = req.body.code
   if (!code) {
-    console.log('params', req.params)
     return res.status(400).send('Bad Request');
   }
-  strava.oauth.getToken(code, function(data) {
-    console.log('getToken', data)
+  console.log('getToken '+code)
+  strava.oauth.getToken(code, function(err, payload, limits) {
+    console.log('getToken', payload.access_token)
+    req.session.access_token = payload.access_token
     res.send({status: 'ok'})
-    strava.athlete.get({},function(err, payload, limits) {
-      if(!err) {
-        console.log(payload);
-      }
-      else {
-        console.log(err);
-      }
-    });
   })
 })
 
+app.get('/api/athlete', (req, res) => {
+  const access_token = req.session.access_token
+  strava.athlete.get({access_token},function(err, payload, limits) {
+    if(!err) {
+      console.log(payload);
+      res.send(payload)
+    }
+    else {
+      console.log(err);
+      res.send(err)
+    }
+  });
+})
+
 app.get('/api/logout', (req, res) => {
-  strava.oauth.deauthorize({}, (data) => {
+  const access_token = req.session.access_token
+  strava.oauth.deauthorize({access_token}, (data) => {
     res.send({status: 'ok'})
   })
 })
 
 app.get('/api', (req,res) => {
   console.log('/api')
-
-    strava.athletes.get({id:4516293},function(err,payload) {
-      if(!err) {
-        res.send(payload);
-        console.log(payload);
-      }
-      else {
-        console.log(err);
-      }
-	  })
 })
 
 // Always return the main index.html, so react-router render the route in the client
